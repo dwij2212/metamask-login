8dd87a5cdef438f2f6fe2f9efeca1d50fd58550d
dwij2212 <dwij.mehta@gmail.com>
2022-02-01 01:57:20 +0530

    working!

--- api/views.py
+++ api/views.py
@@ -1,9 +1,12 @@
+from tkinter import E
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from .models import MetamaskUser
 # Create your views here.
 from django.http import HttpResponse
 import json
+from django.core.exceptions import ObjectDoesNotExist
+from django.core import serializers
 
 def home(request):
     request.user = None
@@ -15,13 +18,12 @@ def auth(request):
     if request.method == 'POST':
         addr = request.POST.get('addr')
         try:
-            user = MetamaskUser.objects.all()
-            print(user)
-            request.user = user
-            response = {'status': 1, 'message': user} 
+            user = MetamaskUser.objects.get(address=addr)
+            request.user = user.name
+            response = {'status': 1, 'message': user.name} 
             return HttpResponse(json.dumps(response), content_type='application/json')
-        except:
-            print("User not found")
+
+        except ObjectDoesNotExist as e:
             response = {'status': 0, 'message': "No such user!"} 
             return HttpResponse(json.dumps(response), content_type='application/json')
 
