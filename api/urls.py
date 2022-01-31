0b50eac90cdc94aa1fb149aa098b28e9b7b79096
dwij2212 <dwij.mehta@gmail.com>
2022-02-01 01:46:05 +0530

    styling done but messed up functionality

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('auth/', views.auth, name='auth'),
+    path('err/', views.err, name='err'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,25 +2,33 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from .models import MetamaskUser
 # Create your views here.
+from django.http import HttpResponse
+import json
 
 def home(request):
+    request.user = None
     return render(request, 'index.html')
 
 @csrf_exempt
 def auth(request):
 
     if request.method == 'POST':
-        print("addr", request.POST.get('addr'))
         addr = request.POST.get('addr')
-
         try:
-            user = MetamaskUser.objects.get(address=str(addr))
+            user = MetamaskUser.objects.all()
+            print(user)
             request.user = user
-            return render(request, 'auth.html', {'user': user.name})
+            response = {'status': 1, 'message': user} 
+            return HttpResponse(json.dumps(response), content_type='application/json')
         except:
-            return render(request, 'auth.html', {"error": "User not found"})
+            print("User not found")
+            response = {'status': 0, 'message': "No such user!"} 
+            return HttpResponse(json.dumps(response), content_type='application/json')
 
     else:
-        return render(request, 'auth.html', {"user": request.user})    
+        return render(request, 'auth.html', {"user": request.user})   
+
+def err(request):
+    return render(request, 'err.html') 
 
     
